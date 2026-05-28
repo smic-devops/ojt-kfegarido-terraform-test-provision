3b262baf3d192d708a4eeec086a30250289f0e70
Kurt <ojt.kfegarido@sminvestments.com>
2026-05-28 11:36:07 +0800

    formatted tf files

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 resource "aws_instance" "my_ec2" {
-  ami           = "ami-0b4dd9d65556cac22" 
+  ami           = "ami-0b4dd9d65556cac22"
   instance_type = "t3.medium"
 
   subnet_id = data.aws_subnet.existing.id
 
   tags = {
-    Name = "Terraform-EC2"
+    Name        = "Terraform-EC2"
     environment = "Sandbox"
   }
 }
@@ -22,7 +22,7 @@ data "aws_subnet" "existing" {
 
 # s3 bucket 
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = "interns-bucket-testing-1234567890"  
+  bucket = "interns-bucket-testing-1234567890"
 
   tags = {
     Name        = "MyBucket"
